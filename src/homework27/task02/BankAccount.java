a3bdd9908acebf0619f41c0684100a8287d31398
Yuliia Soloviova <solov81yova@gmail.com>
2023-11-13 11:09:06 +0100

    add solution of task01_27

---
+++ src/homework27/task02/BankAccount.java
@@ -0,0 +1,36 @@
+package homework27.task02;
+
+public class BankAccount {
+
+  private int amountOfMoney;// сумма денег на счете (баланс)
+  private final String accountNumber;// текущий номер счета
+
+  // конструктор банковского счета
+  public BankAccount(int amountOfMoney, String accountNumber) {
+    this.amountOfMoney = amountOfMoney;
+    this.accountNumber = accountNumber;
+  }
+
+  public int getAmountOfMoney() {
+    return amountOfMoney;
+  }
+
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+
+// метод снятия денег со счета
+  public int withdrawMoney(int amount) {
+    if (amount < 0) {
+      System.out.println("Error! Check your balance.");
+    }
+    return 0;
+  }
+  // метод пополнения баланса счета
+  public void putMoney(int amount) {
+    if (amount > amountOfMoney) {
+      amountOfMoney += amount;
+      System.out.println("Your balance now is: " + amount);
+    }
+  }
+}
