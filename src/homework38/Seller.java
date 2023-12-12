e6285fac06289b5d1fdd7a1bd2cd05b952ae00e5
Yuliia Soloviova <solov81yova@gmail.com>
2023-12-12 14:47:01 +0100

    add changes homework38

--- src/homework38/Seller.java
+++ src/homework38/Seller.java
@@ -1,29 +1,26 @@
 package homework38;
 
-public class Seller {
-private Product[] products;
+public class Seller extends Product {
+
+  private Product[] products;
 
   public Seller() {
     products = new Product[10];
-    String[] products = {"Cheese", "Rice", "Tee", "Coffee", "Butter", "Milk", "Potato", "Apples",
-        "Sausage", "Meat"};
-    }
+  }
 
   public void greeting() {
     System.out.println("Good day! What would you like to buy?");
-    System.out.println("We have the next products: " + products.length);
-    for (int i = 0; i < products.length; i++) {
-      System.out.print(products[i] + ", ");
-
-    }
   }
-  public void sayPrice(String name, int quantity ) {
-    for (int i = 0; i < products.length; i++) {
-      if (products[i] == products[i]) {
 
+  public double sayPrice(String name, int quantity) {
+    for (int i = 0; i < products.length; i++) {
+      if (this.products == products && this.getQuantity() >= quantity) {
+        return getPrice();
+      }
+      }
+        return -1;
       }
 
     }
 
-  }
-}
+
