fa9c19966f23a3beca01d09d06116abda9e6e490
Yuliia Soloviova <solov81yova@gmail.com>
2024-01-17 12:28:32 +0100

    add task01 solution hw49

---
+++ src/homework49/task01/Main.java
@@ -0,0 +1,36 @@
+package homework49.task01;
+/*
+Взять ранее разработанный класс-коллекцию MyArray из пакета homework в текущем занятии.
+Во всех случаях, где в методах класса происходят какие-то исключительные ситуации,
+из-за которых метод не может дальше работать, выкидывать exception соответствующего типа.
+Например, если указан отрицательный индекс, выкидывать IndexOutOfBoundsException.
+
+Также создать класс Main и в методе main продемонстрировать три случая, где из объекта класса MyArray
+будет выкинуто исключение. Словить и обработать эти исключения.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    MyArray<Integer> myArray = new MyArray<>();
+    try {
+      myArray.set(1, 9);
+    } catch (IndexOutOfBoundsException ex) {
+      System.out.println(ex.getMessage());
+    }
+    try {
+      myArray.indexOf(2);
+    } catch (RuntimeException ex) {
+      System.out.println(ex.getMessage());
+    }
+    try {
+      myArray.add(3, 7);
+    } catch (IndexOutOfBoundsException ex) {
+      System.out.println(ex.getMessage());
+    }
+    try {
+      myArray.remove(4);
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      System.out.println(ex.getMessage());
+    }
+  }
+}
